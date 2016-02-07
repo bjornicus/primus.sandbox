da773cab47f1f04b88813f246dc25844b03595d1
Bjorn Hansen <holomorph@gmail.com>
2016-02-06 23:40:39 -0800

    borrow the facebook example

--- public/chat.jsx
+++ public/chat.jsx
@@ -33,64 +33,102 @@
 // };
 
 
-//
-// react stuff
-//
-var mockData = [
-{"author": "Pete Hunt", "text": "This is one comment"},
-{"author": "Jordan Walke", "text": "This is *another* comment"}
-]
 
 var Comment = React.createClass({
-    render: function() {
-        return ( 
-            <div className = "comment">
-            <h2 className = "commentAuthor"> 
-            {this.props.author} 
-            </h2> 
-            {this.props.children} 
-            </div>
-            );
-    }
+  render: function() {
+    return (
+      <div className="comment">
+      <h2 className="commentAuthor">
+      {this.props.author}
+      </h2>
+      <span>{this.props.children}</span>
+      </div>
+      );
+}
 });
-var CommentList = React.createClass({
-    render: function() {
-        var commentNodes = this.props.data.map(function(comment) {
-            return ( 
-                <Comment author={comment.author} key={comment.id}> 
-                {comment.text} 
-                </Comment>
-                );
-        });
-        return ( 
-            <div className="commentList"> {commentNodes} </div>
-            );
-    }
-});
-var CommentForm = React.createClass({
-    render: function() {
-        return ( 
-            <div className="commentForm">
-            Hello, world!I am a CommentForm. 
-            </div>
-            );
-    }
-});
-var CommentBox = React.createClass({  
+
+var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+        //this.setState({data: data});
+    },
+    handleCommentSubmit: function(comment) {
+        // stuff
+    },
     getInitialState: function() {
         return {data: []};
-    },  
+    },
     componentDidMount: function() {
-        this.setState({data: mockData});
+        //this.loadCommentsFromServer();
     },
     render: function() {
-        return ( 
-            <div className="commentBox">
-            <h1> Comments </h1> 
-            <CommentList data={this.state.data} />
-            <CommentForm/>
-            </div>
-            );
+        return (
+          <div className="commentBox">
+          <h1>Comments</h1>
+          <CommentList data={this.state.data} />
+          <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+          </div>
+          );
     }
 });
-ReactDOM.render( <CommentBox/>, document.getElementById('content') );
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+        {comment.text}
+        </Comment>
+        );
+  });
+    return (
+      <div className="commentList">
+      {commentNodes}
+      </div>
+      );
+}
+});
+
+var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''};
+},
+handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+},
+handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+},
+handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+  }
+  this.props.onCommentSubmit({author: author, text: text});
+  this.setState({author: '', text: ''});
+},
+render: function() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+      <input
+      type="text"
+      placeholder="Your name"
+      value={this.state.author}
+      onChange={this.handleAuthorChange}/>
+      <input
+      type="text"
+      placeholder="Say something..."
+      value={this.state.text}
+      onChange={this.handleTextChange}/>
+      <input type="submit" value="Post" />
+      </form>
+      );
+}
+});
+
+ReactDOM.render(
+  <CommentBox url="/api/comments" pollInterval={2000} />,
+  document.getElementById('content')
+  );
+
